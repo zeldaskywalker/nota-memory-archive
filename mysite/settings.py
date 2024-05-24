18fbb8b856ab03866cc96fa294b7e31e8158a1aa
zelda <126986145+zeldaskywalker@users.noreply.github.com>
2024-05-23 22:42:03 -0400

    fix staticfiles?

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n%%cm4z^s#&)hmw)jf2vko7^eoeoxy+1t!15iwy-(b%+pzskf5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
