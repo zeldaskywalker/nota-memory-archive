f342fe869351b00e3781eed86e57c05ea9ef78ea
zelda <126986145+zeldaskywalker@users.noreply.github.com>
2024-05-23 23:20:10 -0400

    lol small error

--- build.sh
+++ build.sh
@@ -7,6 +7,3 @@ pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
-python manage.py migrate
--- mysite/settings.py
+++ mysite/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = 'RENDER'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
